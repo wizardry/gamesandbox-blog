b20240f237b699dbcc79fa8df04645abe196a391
wizardry <rein1999@gmail.com>
2014-12-22 13:08:53 +0900

    script調整

---
+++ build/script/common-acae6e99.js
@@ -0,0 +1,1 @@
+$(function(){function t(){var t="/gamesandbox-blog/build";$("a[href^=/]").each(function(){$(this).attr("href",t+$(this).attr("href"))})}t()});
--- source/script/common.js
+++ source/script/common.js
@@ -1,10 +1,9 @@
 $(function(){
-
 	function linkOverWriten(){
 		var ROOTPATH = '/gamesandbox-blog/build';
 		$('a[href^=/]').each(function(){
 			$(this).attr('href',ROOTPATH+$(this).attr('href'));
 		})
 	}
-
+	linkOverWriten()
 })
